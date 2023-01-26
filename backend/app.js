025541a4ed604ab374bbaf62e0d83b04719243a5
Aydar <sekret39-01@inbox.ru>
2023-01-26 15:56:38 +0200

    проверка 2

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ async function connect() {
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  standardHeaders: true,
   legacyHeaders: false,
 });
 
