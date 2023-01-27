eb56c5f0a73e79f09de7b5115e6bc2b282b14e3e
Aydar <sekret39-01@inbox.ru>
2023-01-27 13:29:53 +0200

    поменял сервер бэка

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ async function connect() {
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 100,
   standardHeaders: true,
   legacyHeaders: false,
 });
