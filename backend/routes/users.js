01d4c19cc6c32dfaeebcdfa40db6b19565f081b9
Aydar <sekret39-01@inbox.ru>
2023-04-14 10:54:46 +0200

    исправил Readme

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -19,4 +19,5 @@ userRouter.get('/me', getUserMe);
 userRouter.get('/:userId', validationFindUserById, getUserById);
 userRouter.patch('/me', validationUpdateUserInfo, updateUser);
 userRouter.patch('/me/avatar', validationUpdateUserAvatar, updateAvatar);
+
 module.exports = userRouter;
